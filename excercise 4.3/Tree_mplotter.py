87efcfac5c69f5113ef042b915c65892ec92486f
yrudreLau <64315349+yrudreLau@users.noreply.github.com>
2020-08-06 00:15:45 +0800

    Add files via upload

---
+++ excercise 4.3/Tree_mplotter.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 23:30:45 2020
+
+@author: 交通流小组
+"""
+
+import matplotlib.pyplot as plt
+from pylab import *
+
+DeciNode = dict(boxstyle = 'square, pad = 0.5', fc = '0.9')
+LeafNode = dict(boxstyle = 'round4, pad = 0.5', fc = '0.9')
+arrow_args = dict(arrowstyle = '<|-', connectionstyle="arc3", shrinkA=0,
+                  shrinkB=16)
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+
+def plotNode(Nodename, cenCoor, priorCoor, nodeType):  
+    '''
+    centerCoor: Node center coordinates
+    priorPt: Starting point coordinates
+    '''
+    createFig.ax1.annotate(Nodename, xy = priorCoor, 
+                            xycoords = 'axes fraction', xytext = cenCoor, 
+                            textcoords = 'axes fraction', va = 'center', 
+                            ha = 'center', bbox = nodeType, arrowprops = arrow_args)## plot charaters
+    
+def getLeafNum(Deci_Tree):
+    '''
+    Deci_Tree: dict, get by function DecisionTreeRecursive
+    return number of leaf nodes
+    '''
+    
+    ## initialization
+    Leaf_Num = 0
+    ## get the decision node
+    RootNode = list(Deci_Tree.keys())[0] ## get root node
+    BranchNodedict = Deci_Tree[RootNode] ## get the values of root node
+    for key in BranchNodedict.keys():
+        if type(BranchNodedict[key]).__name__ == 'dict': ## Determine whether the value is still a dictionary
+            Leaf_Num += getLeafNum(BranchNodedict[key])
+        else:
+            Leaf_Num += 1
+    
+    return Leaf_Num
+
+def getFloorNum(Deci_Tree):
+    
+    ## initialization
+    maxFloor = 0    
+#    currentFloor = 0
+    RootNode = list(Deci_Tree.keys())[0] ## get root node
+    BranchNodedict = Deci_Tree[RootNode] ## get the values of root node
+    for key in BranchNodedict.keys():
+        if type(BranchNodedict[key]).__name__ == 'dict': ## Determine whether the value is still a dictionary
+            currentFloor = 1 + getFloorNum(BranchNodedict[key])
+        else:
+            currentFloor = 1
+        if currentFloor > maxFloor:
+            maxFloor = currentFloor
+            
+    return maxFloor
+
+
+
+## define plot text function
+def plotText(cenCoor, priorCoor, txtString):
+    txtCoor_x = (priorCoor[0] + cenCoor[0]) / 2.0 # (priorCoor[0] - cenCoor[0]) / 2.0 + cenCoor[0] #
+    txtCoor_y = (priorCoor[1] + cenCoor[1]) / 2.0 # (priorCoor[1] - cenCoor[1]) / 2.0 + cenCoor[1] #
+    createFig.ax1.text(txtCoor_x, txtCoor_y, txtString)
+    
+def plotTree(Deci_Tree, priorCoor, nodeName):
+    LeafNum = getLeafNum(Deci_Tree)
+    Floor = getFloorNum(Deci_Tree)
+    RootNode = list(Deci_Tree.keys())[0]
+    cenCoor = (plotTree.xoff + (1.0 + float(LeafNum)) / 2.0 / plotTree.totalw,
+              plotTree.yoff)
+#    print(plotTree.yoff)
+    plotText(cenCoor, priorCoor, nodeName)
+    plotNode(RootNode, cenCoor, priorCoor, DeciNode)
+    BranchNodeDict = Deci_Tree[RootNode]
+    plotTree.yoff = plotTree.yoff - 1.0 / plotTree.totalh
+#    print('1min = ',plotTree.yoff)
+    for key in BranchNodeDict.keys():
+        if type(BranchNodeDict[key]).__name__ == 'dict':
+            plotTree(BranchNodeDict[key], cenCoor, str(key))
+        else:
+            plotTree.xoff = plotTree.xoff + 1.0 / plotTree.totalw
+            plotNode(BranchNodeDict[key], (plotTree.xoff, plotTree.yoff), 
+                     cenCoor, LeafNode)
+            plotText((plotTree.xoff, plotTree.yoff), cenCoor, str(key))
+    plotTree.yoff = plotTree.yoff + 1.0 / plotTree.totalh
+#    print('2 min = ', plotTree.yoff)
+    
+def createFig(DecisionTree):
+    fig = plt.figure(1, facecolor = 'white')
+    fig.clf() ## clear figure plot area
+    axprops = dict(xticks = [], yticks = [])
+    createFig.ax1 = plt.subplot(111, frameon = False, **axprops)
+    plotTree.totalw = float(getLeafNum(DecisionTree))
+#    print('w=', plotTree.totalw)
+    plotTree.totalh = float(getFloorNum(DecisionTree))
+#    print('h=', plotTree.totalh)
+    plotTree.xoff = -0.5 / plotTree.totalw
+    plotTree.yoff = 1.0
+    plotTree(DecisionTree, (0.5, 1.0), '')
+    plt.savefig('DecisionTree.jpg')
+    plt.show()
---
+++ excercise 4.3/Trees_m.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 23:29:44 2020
+
+@author: 交通流小组
+"""
+
+import numpy as np
+import operator
+
+def InformationEntropy(dataSet):
+    Sample_Num = len(dataSet) ##count number of samples
+    
+    ## create a dict to record sample class and sample number of each class
+    Sample_Class = {} 
+    for datum in dataSet:
+        Sample_Lablel = datum[-1]
+        if Sample_Lablel not in Sample_Class.keys():
+            Sample_Class[Sample_Lablel] = 1
+        else:
+            Sample_Class[Sample_Lablel] += 1
+    
+    ## calculate information entropy
+    Ent_D = 0
+    for key in Sample_Class:
+        p_k = Sample_Class[key] / Sample_Num
+        Ent_D -= p_k*np.log2(p_k)
+        
+        
+    return Sample_Class, Ent_D
+
+## split dataset by attribute return subset of input attribute value
+def splitDataSet(dataSet, attri_index, attri_value):
+    retDataSet = []
+    featVec = []
+    for featVec in dataSet:
+        if featVec[attri_index] == attri_value:
+#             print(featVec)
+            reducedFeatVec = featVec[:attri_index]
+#             print(reducedFeatVec)
+            reducedFeatVec += featVec[attri_index + 1:]
+#             print(b)
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+## calculate optimal information Gain and choose optimal subset split mode
+def optimal_attributeIndex(dataSet):
+    ## get the attribute
+    attribute_Num = len(dataSet[0]) - 1
+    datasetInfEnt_D = InformationEntropy(dataSet)[-1]
+    
+    ## initialization
+    optimal_InfGain_Da = 0
+    optimal_attribute = -1
+    
+    ## calculate each attribute's information gain
+    for i in range(attribute_Num):
+        data_attribute = [data_attri[i] for data_attri in dataSet]
+        data_values = set(data_attribute)
+        attributeEntropy = 0
+        for value in data_values:
+            subDataSet = splitDataSet(dataSet, i, value)
+            p_k = len(subDataSet) / float(len(dataSet))
+            attributeEntropy += p_k * InformationEntropy(subDataSet)[-1]
+        InfGain_Da = datasetInfEnt_D - attributeEntropy
+#         print(InfGain_Da)
+        if (InfGain_Da > optimal_InfGain_Da): ## choose optimal attribute
+            optimal_InfGain_Da = InfGain_Da
+            optimal_attribute = i
+            
+    return optimal_attribute
+
+## return the major sample class by sorting
+def majorSampleClass(ClassList):
+    ClassCount = {}
+    for datum in ClassList:
+#         print(datum)
+        if datum not in ClassCount.keys():
+            ClassCount[datum] = 1
+        else:
+            ClassCount[datum] += 1
+    sortedClassCount = sorted(ClassCount.items(), key = operator.itemgetter(1), reverse = True)
+    
+    return sortedClassCount[0][0]
+
+## define decision tree recursive function
+def DecisionTreeRecursive(dataSet, attributes):
+    ClassList = [datum[-1] for datum in dataSet]
+    if ClassList.count(ClassList[0]) == len(ClassList): ## if there is only one class, return, else return major class
+        return ClassList[0]
+    if len(dataSet[0]) == 1:
+        return majorSampleClass(ClassList)
+    optimal_attri = optimal_attributeIndex(dataSet) ## get index of optimal attribute 
+    optimal_attriLabel = attributes[optimal_attri]
+    Deci_Tree = {optimal_attriLabel:{}}
+    del (attributes[optimal_attri]) ## delete attribute already chosen
+    attributeValues = [datum[optimal_attri] for datum in dataSet]
+    for value in attributeValues:
+        subLabels = attributes[:]
+        Deci_Tree[optimal_attriLabel][value] = DecisionTreeRecursive(
+            splitDataSet(dataSet, optimal_attri, value), subLabels)
+        
+    return Deci_Tree
